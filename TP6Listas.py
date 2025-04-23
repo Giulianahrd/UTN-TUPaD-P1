8f7cd3ee8d16fda0dd6ac879ade0fb90449e4616
Giulianahrd <143147492+Giulianahrd@users.noreply.github.com>
2025-04-22 23:17:55 -0300

    Add files via upload

---
+++ TP6Listas.py
@@ -0,0 +1,66 @@
+# # 1) 
+# multiplos_de_4 = list(range(4, 101, 4))
+# print(multiplos_de_4)
+
+
+# # 2)
+# list = [1, 8.9, True, 'string', 3]
+# print(list[3])
+
+
+# # 3)
+# lista_vacia = []
+# lista_vacia.append('uno')
+# lista_vacia.append('dos')
+# lista_vacia.append('tres')
+# print(lista_vacia)
+
+
+# # 4)
+# animales = ["perro", "gato", "conejo", "pez"]
+# animales[1] = 'loro'
+# animales[3] = 'oso'
+# print(animales)
+
+
+# # 5)
+# list.remove(max(list))
+# Siendo list una lista .remove elimina un elemento de esta, pero el max indica que se debe eliminar el número más grande de la lista. Por lo tanto en el ejemplo, se devuelve una lista sin el número 22.
+
+
+# # 6)
+# mult_5 = list(range(10, 31,5))
+# print(mult_5[0:2])
+
+
+# # 7)
+# autos = ["sedan", "polo", "suran", "gol"]
+# autos[1:3] = ['camaro', 'mustang']
+# print(autos)
+
+
+# # 8)
+# dobles = []
+# dobles.append(5 * 2)
+# dobles.append(10 * 2)
+# dobles.append(15 * 2)
+# print(dobles)
+
+
+# # 9)
+# compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+# # a)
+# compras[2].append('jugo')
+# # b)
+# compras[1][1] = 'tallarines'
+# # c)
+# compras[0].remove('pan')
+# # d)
+# print(compras)
+
+
+
+# # 10)
+# lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+# print(lista_anidada)
+
